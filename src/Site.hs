d74a84528fb65937476b0e806ec2fea3fd763970
Gabriel Cholette-Rioux <Showlet95@hotmail.com>
2017-10-22 13:33:14 -0400

    - added postresql database support. - added psql authentification

--- src/Site.hs
+++ src/Site.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 ------------------------------------------------------------------------------
 -- | This module is where all the routes and handlers are defined for your
@@ -10,21 +11,29 @@ module Site
 
 ------------------------------------------------------------------------------
 import           Control.Applicative
+import           Control.Monad.State.Class
 import           Data.ByteString (ByteString)
 import           Data.Map.Syntax ((##))
 import qualified Data.Text as T
+import qualified Heist.Interpreted as I
 import           Snap.Core
+import           Snap.Extras.JSON
 import           Snap.Snaplet
 import           Snap.Snaplet.Auth
 import           Snap.Snaplet.Auth.Backends.JsonFile
+import           Snap.Snaplet.Auth.Backends.PostgresqlSimple
 import           Snap.Snaplet.Heist
+import           Snap.Snaplet.PostgresqlSimple
 import           Snap.Snaplet.Session.Backends.CookieSession
 import           Snap.Util.FileServe
-import qualified Heist.Interpreted as I
 ------------------------------------------------------------------------------
 import           Application
 
+instance HasPostgres (Handler b App) where
+    getPostgresState = with db get
 
+instance HasPostgres (Handler App (AuthManager App)) where
+    getPostgresState = withTop db get
 ------------------------------------------------------------------------------
 -- | Render login form
 handleLogin :: Maybe T.Text -> Handler App (AuthManager App) ()
@@ -32,7 +41,6 @@ handleLogin authError = heistLocal (I.bindSplices errs) $ render "login"
   where
     errs = maybe mempty splice authError
     splice err = "loginError" ## I.textSplice err
-
 
 ------------------------------------------------------------------------------
 -- | Handle login submit
@@ -43,15 +51,17 @@ handleLoginSubmit =
   where
     err = Just "Unknown user or password"
 
-
 ------------------------------------------------------------------------------
 -- | Logs out and redirects the user to the site index.
 handleLogout :: Handler App (AuthManager App) ()
 handleLogout = logout >> redirect "/"
 
 ------------------------------------------------------------------------------
--- | Creates a new task
--- handleNewTask :: Handler App (AuthManager)
+-- | sample postgres query
+getUsersFromPostgres :: Handler App (AuthManager App) ()
+getUsersFromPostgres = do
+    results <- query_ "select * from snap_auth_user"
+    writeJSON (results :: [AuthUser])
 
 ------------------------------------------------------------------------------
 -- | Handle new user form submit
@@ -59,16 +69,36 @@ handleNewUser :: Handler App (AuthManager App) ()
 handleNewUser = method GET handleForm <|> method POST handleFormSubmit
   where
     handleForm = render "new_user"
-    handleFormSubmit = registerUser "login" "password" >> redirect "/"
+    handleFormSubmit = logout
+          >> registerUser "login" "password" 
+          >> handleLoginSubmit
+          >> redirect "/"
 
+hLogout :: Handler App App ()
+hLogout = with auth handleLogout
+
+------------------------------------------------------------------------------
+-- | checks if the user is logged in and routes to the 
+-- | correct function
+restrictedHandler :: Handler App (AuthManager App) ()
+restrictedHandler = requireUser auth noUserHandler userExistsHandler
+
+noUserHandler :: Handler App (AuthManager App) ()
+noUserHandler = writeBS "No user"
+
+userExistsHandler :: Handler App (AuthManager App) ()
+userExistsHandler = writeBS "User Exists"
 
 ------------------------------------------------------------------------------
 -- | The application's routes.
 routes :: [(ByteString, Handler App App ())]
-routes = [ ("login",    with auth handleLoginSubmit)
-         , ("logout",   with auth handleLogout)
-         , ("new_user", with auth handleNewUser)
-         , ("",         serveDirectory "static")
+routes = [ ("/login",      with auth handleLoginSubmit)
+         , ("/logout",     with auth handleLogout)
+         , ("/new_user",   with auth handleNewUser)
+         , ("/hlogout",    hLogout)
+         , ("/restricted", with auth restrictedHandler)
+         , ("/getUsers",        with auth getUsersFromPostgres)
+         , ("",           serveDirectory "static")
          ]
 
 
@@ -79,9 +109,10 @@ app = makeSnaplet "app" "mindsnap" Nothing $ do
     h <- nestSnaplet "" heist $ heistInit "templates"
     s <- nestSnaplet "sess" sess $
            initCookieSessionManager "site_key.txt" "sess" Nothing (Just 3600)
+    d <- nestSnaplet "db" db pgsInit
     a <- nestSnaplet "auth" auth $
-           initJsonFileAuthManager defAuthSettings sess "users.json"
+           initPostgresAuth sess d
     addRoutes routes
     addAuthSplices h auth
-    return $ App h s a
+    return $ App h s d a
 
